ddeaa4ad29e2d31445b52e611a9a2f1e005054c6
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-09-18 23:54:27 +0100

    feat: react routerdom installation and routing for Login and Gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login, Gallery} from './pages'
 
+
+function App() {
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path="/gallery" element={<Gallery />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
